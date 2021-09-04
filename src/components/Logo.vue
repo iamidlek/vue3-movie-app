55cc8ec56874e9e170a03915474e36c860b3cf35
iamidlek <iamidlek@naver.com>
2021-09-04 19:22:40 +0900

    logo comp

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,7 +2,7 @@
   <RouterLink
     to="/"
     class="logo">
-    OMDbAPI.COM
+    <span>OMDbAPI</span>.COM
   </RouterLink>
 </template>
 
@@ -12,5 +12,12 @@
   font-family: "Oswald", sans-serif;
   font-size: 20px;
   color: $black;
+  text-decoration: none;
+  &:hover {
+    color: $black;
+  }
+  span {
+    color: $primary;
+  }
 }
 </style>
