166de96f913607b3a1ce345694d195e73a87ddc5
iamidlek <iamidlek@naver.com>
2021-09-04 19:05:36 +0900

    create logo vue

---
+++ src/components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <RouterLink
+    to="/"
+    class="logo">
+    OMDbAPI.COM
+  </RouterLink>
+</template>
+
+<style lang="scss" scoped>
+@import "~/scss/style";
+.logo {
+  font-family: "Oswald", sans-serif;
+  font-size: 20px;
+  color: $black;
+}
+</style>
