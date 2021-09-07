017b882c348e1170508111bc3335355a6e2f31e9
iamidlek <iamidlek@naver.com>
2021-09-08 00:29:32 +0900

    add vuex

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -3,7 +3,8 @@
     <div class="inner">
       <MovieItem
         v-for="movie in movies"
-        :key="movie.imdbID" />
+        :key="movie.imdbID"
+        :movie="movie" />
     </div>
   </div>
 </template>
@@ -15,9 +16,15 @@ export default {
   components: {
     MovieItem
   },
-  data() {
-    return {
-      movies: []
+  // data() {
+  //   return {
+  //     movies: []
+  //   }
+  // }
+  computed: {
+    movies() {
+      // 스토어 데이터 모듈 값 // 값의 반응성을 위해 computed에서 사용
+      return this.$store.state.movie.movies
     }
   }
 }
