19df3c673e4291afab2d912249e2a0dc1187fc49
iamidlek <iamidlek@naver.com>
2021-09-08 00:35:31 +0900

    add store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App'
-import router from './routes/index.js'
+// import router from './routes/index.js'
+
+// index.js는 생략해도 알아서 가져옴
+// 새로운 폴더에 index.js로 만들면 편함
+import router from './routes'
+// import store from './store/index.js'
+import store from './store'
 // import '~/scss/style.scss'
 
 createApp(App)
   .use(router)
+  .use(store)
   .mount('#app')
