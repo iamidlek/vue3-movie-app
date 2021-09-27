913130e51b5683960c94508deb8b42acbb543881
iamidlek <iamidlek@naver.com>
2021-09-27 23:20:46 +0900

    feat: create footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <Logo />
+    <a 
+      href="https://github.com/iamidlek"
+      target="_blank">
+      (c){{ new Date().getFullYear() }} Yanagi 
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: .3;
+  .logo {
+    // Logo의 RouterLink 는 a 태그로 변환됨
+    display: block;
+    margin-bottom: 4px;
+  }
+}
+</style>
