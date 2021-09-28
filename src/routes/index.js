e12329cdc1761a07c8e9357c8aed5fda1e223abd
iamidlek <iamidlek@naver.com>
2021-09-28 19:04:09 +0900

    feat: 404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from './Home'
 import Movie from './Movie'
 import About from './About'
-
+import NotFound from './NotFound'
 
 export default createRouter({
   // hash, history 모드
@@ -21,5 +21,9 @@ export default createRouter({
       path: '/about',
       component: About
     },
+    {// pathMatch는 notFound 와 같이 아무거나 입력 가능
+      path: '/:pathMatch(.*)',
+      component: NotFound
+    }
   ]
 })
