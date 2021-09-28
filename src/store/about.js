7f9973ead5de11e4bb6a817e8376ba67d504d2ef
iamidlek <iamidlek@naver.com>
2021-09-28 18:45:16 +0900

    feat: about page

---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+// store로 자료 작성
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'yoo',
+    email: 'iamidlek@naver.com',
+    blog: 'none',
+    phone: '+82-01-1234-5678',
+    image: 'none'
+  })
+}
